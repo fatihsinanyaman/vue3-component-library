43104044a906eed737ecbf14e47b294449ee75c0
Sinan Yaman <fatihsinanyaman@gmail.com>
2021-05-02 18:24:51 +0300

    npm configurations

--- scripts/templates/packageJson.js
+++ scripts/templates/packageJson.js
@@ -1,8 +1,25 @@
 module.exports =  ({ packageName }) => {
   return `{
     "name": "@squanch-vue3/${packageName}",
-    "description": "Sandbox Component",
-    "version": "0.0.5",
+    "description": "Vue3 Component Library Component Example for ${packageName}",
+    "version": "0.0.0",
+    "repository": {
+        "type": "git",
+        "url": "git+https://github.com/fatihsinanyaman/vue3-component-library.git"
+    },
+    "keywords": [
+        "vue3",
+        "component",
+        "library",
+        "vue3",
+        "lerna",
+        "storybook"
+    ],
+    "homepage": "https://github.com/fatihsinanyaman/vue3-component-library#readme",
+    "bugs": {
+        "url": "https://github.com/fatihsinanyaman/vue3-component-library/issues"
+    },
+    "author": "Fatih Sinan Yaman",
     "main": "./dist/@squanch-vue3/${packageName}.umd.js",
     "module": "./dist/@squanch-vue3/${packageName}.es.js",
     "exports": {
