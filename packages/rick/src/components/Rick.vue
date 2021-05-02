09ad44a8fb91f9f624e529062d963a19bc45e7e9
Sinan Yaman <fatihsinanyaman@gmail.com>
2021-05-02 15:34:01 +0300

    test

--- packages/rick/src/components/Rick.vue
+++ packages/rick/src/components/Rick.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Rick Sanchez!!</h1>
+        <h1>Rick Sanchez</h1>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez">
     </div>
 </template>
